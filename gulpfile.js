f10715a764805cd38bc98fb9ec8b249ddb4d9627
Jeff Eoff <jeffeoff@gmail.com>
2019-04-04 05:22:50 -0500

    stripped csscomb out, gulp lint only lints now.

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,10 @@ var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var sassGlob = require('gulp-sass-glob');
 var sassLint = require('gulp-sass-lint');
-var cssComb = require('gulp-csscomb');
+
 
 var sassDest = 'sass/';
 var cssDest = 'css/';
-var cssProdDest = 'prod/css';
 var sassMain = sassDest +'main.scss';
 
 // get all sass files but exclude any vendor sass inside of sass/
@@ -20,6 +19,7 @@ var sassSrc = [sassDest +'**/*.s+(a|c)ss', '!'+ sassDest +'/vendor/**'];
 // same as above, but also exclude main.scss and _settings.scss so
 // comb doesn't insert hard imports for every scss file.
 var sassSrcComb = [sassDest +'**/*.s+(a|c)ss', '!'+ sassMain, '!'+ sassDest +'_settings.scss', '!'+ sassDest +'/vendor/**'];
+
 
 gulp.task('default', ['css']);
 
@@ -45,36 +45,6 @@ gulp.task('lint', function () {
         configFile: './.sass-lint.yml'
 		}) )
 		.pipe( sassLint.format() )
-		.pipe( sassLint.failOnError() )
-		.pipe( sourcemaps.init() )
-		.pipe( sass({outputStyle: 'expanded'}).on('error', sass.logError) )
-		.pipe( autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false }) )
-		.pipe( sourcemaps.write('./sourcemaps') )
-		.pipe( gulp.dest(cssDest) );
-});
-
-
-gulp.task('comb', function() {
-  return gulp.src( sassSrcComb )
-    .pipe( sassGlob() )
-    .pipe( cssComb() )
-    .pipe( gulp.dest( sassDest ) );
-});
-
-
-gulp.task('compile', ['comb', 'lint']);
-
-
-gulp.task('prod', function () {
-	return gulp.src( sassMain )
-    .pipe( sassGlob() )
-		.pipe( sass({outputStyle: 'compressed'}).on('error', sass.logError) )
-		.pipe( autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false }) )
-		.pipe( gulp.dest(cssProdDest) );
 });
 
 
@@ -90,7 +60,6 @@ gulp.task('css-reload', function () {
 		.pipe( gulp.dest(cssDest) )
 		.pipe( livereload() );
 });
-
 
 gulp.task('livereload', ['watch-reload']);
 gulp.task('reload', ['watch-reload']);
